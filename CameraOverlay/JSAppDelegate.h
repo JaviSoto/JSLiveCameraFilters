9c7af3d84dc09d110ef07207d68780a602f62e2e
Javier Soto <javier.api@gmail.com>
2012-03-19 15:14:36 +0100

    Initial Commit

---
+++ CameraOverlay/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  CameraOverlay
+//
+//  Created by Javier Soto on 3/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
